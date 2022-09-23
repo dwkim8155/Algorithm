94bbc672eabbff4ae548eb9ee7f29e3a8dc1718e
Dongwoo Kim <dwkim8155@naver.com>
2022-09-23 18:24:54 +0900

    [BoJ] 10773 Sliver4 제로

---
+++ Algorithm/Data Structure/[BoJ] 10773 제로.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+stack = []
+
+for _ in range(N):
+    num = int(input())
+    if num == 0 :
+        stack.pop()
+    else:
+        stack.append(num)
+
+print(sum(stack))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
