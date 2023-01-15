a403d175817c4a679195a80d357806f29ba828a7
Dongwoo Kim <dwkim8155@naver.com>
2023-01-15 12:18:25 +0900

    [programmers] level2 큰 수 만들기

---
+++ Algorithm/Data Structure/[programmers] level2 큰 수 만들기.py
@@ -0,0 +1,13 @@
+def solution(number, k):
+    
+    stack  = []
+    
+    for i in number:
+        
+        while k >0 and stack and stack[-1] < i:
+            stack.pop()
+            k-=1
+        stack.append(i)
+    
+    
+    return ''.join(stack[:len(stack)-k])
